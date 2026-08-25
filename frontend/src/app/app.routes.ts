1b3a404bf7d538243921dd0855d485726edcb193
leo377168-prog <leo377168@gmail.com>
2026-08-25 14:54:11 +0000

    progetto2_finito

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import {ListaProdottiComponent} from './lista-prodotti/lista-prodotti.component';
 import {DettaglioProdottoComponent} from './dettaglio-prodotto-component/dettaglio-prodotto-component.component';
-import {CarrelloComponentComponent} from './carrello-component/carrello-component.component';
+import {CarrelloComponent} from './carrello-component/carrello-component.component';
 
 export const routes: Routes = [
   { path: '', component: ListaProdottiComponent },
   { path: 'dettaglio/:id', component: DettaglioProdottoComponent },
-  { path: 'carrello', component: CarrelloComponentComponent },
+  { path: 'carrello', component: CarrelloComponent },
 ];
 
 
--- frontend/src/app/carrello-component/carrello-component.component.ts
+++ frontend/src/app/carrello-component/carrello-component.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CartServiceService } from '../cart-service.service';
+import { ProdottoModel } from '../models/prodotto.model';
 
 @Component({
-  selector: 'app-carrello-component',
+  selector: 'app-carrello',
   standalone: true,
-  imports: [],
-  templateUrl: './carrello-component.component.html',
-  styleUrl: './carrello-component.component.css'
+  imports: [RouterLink],
+  templateUrl: './carrello-component.component.html'
 })
-export class CarrelloComponentComponent {
+export class CarrelloComponent {
+  private cartService = inject(CartServiceService);
 
+  // Getter per accedere facilmente all'array del servizio
+  get items(): ProdottoModel[] {
+    return this.cartService.getCart();
+  }
+
+  get totale(): number {
+    return this.cartService.totalPrice();
+  }
+
+  svuota(): void {
+    this.cartService.clearCart();
+  }
 }
